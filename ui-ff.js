05924b78f870be26119e879a130529de9f9bfd2a
Adam Zeffer <adam.zeffer@drivetime.com>
2024-05-14 08:08:32 -0700

    chore: adding require stmt

--- ui-ff.js
+++ ui-ff.js
@@ -1,32 +1,37 @@
 function uiFeatureFlags () {
     const flags = ['adam_poc_ui', 'adams_poc_ui_button']
+    require(['https://unpkg.com/launchdarkly-js-client-sdk@3'], function(LDClient) {
+        // Now you can use LDClient here
+        // For example:
+        // Use client as needed
+        // LaunchDarkly SDK script has finished loading
+        // Now you can safely use LDClient
+        const context = {
+            kind: 'user',
+            key: 'example-user-key',
+            name: 'Sandy',
+        }
+        const ldclient = LDClient.initialize('659c257fd655c910832acbaf', context)
 
-    // LaunchDarkly SDK script has finished loading
-    // Now you can safely use LDClient
-    const context = {
-        kind: 'user',
-        key: 'example-user-key',
-        name: 'Sandy',
-    }
-    const ldclient = LDClient.initialize('659c257fd655c910832acbaf', context)
-
-    function render() {
-        flags.forEach((flagKey) => {
-            const elements = document.getElementsByClassName(flagKey)
-            if(!elements || elements.length === 0) {
-                return
-            }
-            for(let i = 0; i < elements.length; i++) {
-                const element = elements[i]
-                const flagValue = ldclient.variation(flagKey, true)
-                if (element) {
-                    element.style.display = flagValue ? 'block' : 'none'
+        function render() {
+            flags.forEach((flagKey) => {
+                const elements = document.getElementsByClassName(flagKey)
+                if(!elements || elements.length === 0) {
+                    return
                 }
-            }
-        })
-    }
+                for(let i = 0; i < elements.length; i++) {
+                    const element = elements[i]
+                    const flagValue = ldclient.variation(flagKey, true)
+                    if (element) {
+                        element.style.display = flagValue ? 'block' : 'none'
+                    }
+                }
+            })
+        }
 
-    ldclient.on('ready', render)
-    ldclient.on('change', render)
+        ldclient.on('ready', render)
+        ldclient.on('change', render)
+    });
+
 }
 uiFeatureFlags()
